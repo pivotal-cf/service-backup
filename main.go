6635ff5960bfe93bb8eb7d4f7bb3a38606eedc03
Rob Dimsdale <rdimsdale@pivotal.io>
2015-06-18 10:42:31 +0100

    Upload provided file to S3.
    [#97180692]
    
    Signed-off-by: Siddhu Warrier <swarrier@pivotal.io>

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("hello world")
+	args := os.Args[1:]
+	cmd := exec.Command(
+		"/var/vcap/packages/aws-cli/bin/aws",
+		"s3",
+		"cp",
+		args[0],
+		args[1],
+		"--endpoint-url",
+		args[2],
+	)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		_ = ioutil.WriteFile(args[3], []byte(err.Error()), 0644)
+
+	}
+	if out != nil {
+		ioutil.WriteFile(args[3], out, 0644)
+	}
 }
