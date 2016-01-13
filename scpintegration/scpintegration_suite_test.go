a67e284f82054283b140afc54b108d0593d3851a
Houssem El Fekih <hfekih@pivotal.io>
2016-01-13 09:56:08 +0000

    backfill integration test for invalid SCP flags
    Signed-off-by: Craig Furman <cfurman@pivotal.io>

--- scpintegration/scpintegration_suite_test.go
+++ scpintegration/scpintegration_suite_test.go
@@ -1,6 +1,15 @@
 package scpintegration_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
@@ -8,19 +17,94 @@ import (
 	"testing"
 )
 
-var pathToServiceBackupBinary string
+const (
+	sshKeyUsername = "service-backup-tmp-key"
+)
+
+type TestData struct {
+	PathToServiceBackupBinary string
+	PrivateKeyPath            string
+	UnixUser                  *user.User
+}
+
+var (
+	pathToServiceBackupBinary string
+	privateKeyPath            string
+	unixUser                  *user.User
+)
+
+func createSSHKey() (string, string) {
+	sshKeys, err := ioutil.TempDir("", "scp-unit-tests")
+	Expect(err).ToNot(HaveOccurred())
+	privateKeyPath = filepath.Join(sshKeys, "id_rsa")
+	Expect(exec.Command("ssh-keygen", "-t", "rsa", "-b", "4096", "-C", sshKeyUsername,
+		"-N", "", "-f", privateKeyPath).Run()).To(Succeed())
+	return filepath.Join(sshKeys, "id_rsa.pub"), privateKeyPath
+}
+
+func addToAuthorizedKeys(publicKeyPath string) {
+	Expect(os.MkdirAll(filepath.Join(unixUser.HomeDir, ".ssh"), 0700)).To(Succeed())
+	authKeys, err := os.OpenFile(filepath.Join(unixUser.HomeDir, ".ssh", "authorized_keys"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	Expect(err).ToNot(HaveOccurred())
+	pubKey, err := os.Open(publicKeyPath)
+	Expect(err).ToNot(HaveOccurred())
+	defer authKeys.Close()
+	defer pubKey.Close()
+	_, err = io.Copy(authKeys, pubKey)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func removeKeyFromAuthorized() {
+	authKeysFilePath := filepath.Join(unixUser.HomeDir, ".ssh", "authorized_keys")
+	authKeysContent, err := ioutil.ReadFile(authKeysFilePath)
+	Expect(err).NotTo(HaveOccurred())
+
+	trimmedAuthKeysLines := [][]byte{}
+	for _, line := range bytes.Split(authKeysContent, []byte("\n")) {
+		if !bytes.Contains(line, []byte(sshKeyUsername)) {
+			trimmedAuthKeysLines = append(trimmedAuthKeysLines, line)
+		}
+	}
+
+	trimmedAuthKeysContent := bytes.Join(trimmedAuthKeysLines, []byte("\n"))
+	err = ioutil.WriteFile(authKeysFilePath, trimmedAuthKeysContent, 0600)
+	Expect(err).NotTo(HaveOccurred())
+}
 
 var _ = SynchronizedBeforeSuite(func() []byte {
+	var publicKeyPath string
+	publicKeyPath, privateKeyPath = createSSHKey()
+
 	var err error
+	unixUser, err = user.Current()
+	Expect(err).NotTo(HaveOccurred())
+
+	addToAuthorizedKeys(publicKeyPath)
+
 	pathToServiceBackupBinary, err = gexec.Build("github.com/pivotal-cf-experimental/service-backup")
 	Expect(err).ToNot(HaveOccurred())
-	return []byte(pathToServiceBackupBinary)
-}, func(binPath []byte) {
-	pathToServiceBackupBinary = string(binPath)
+
+	forOtherNodes, err := json.Marshal(TestData{
+		PathToServiceBackupBinary: pathToServiceBackupBinary,
+		PrivateKeyPath:            privateKeyPath,
+		UnixUser:                  unixUser,
+	})
+	Expect(err).ToNot(HaveOccurred())
+	return forOtherNodes
+}, func(data []byte) {
+	var t TestData
+	Expect(json.Unmarshal(data, &t)).To(Succeed())
+
+	pathToServiceBackupBinary = t.PathToServiceBackupBinary
+	privateKeyPath = t.PrivateKeyPath
+	unixUser = t.UnixUser
 })
 
 var _ = SynchronizedAfterSuite(func() {
 }, func() {
+	removeKeyFromAuthorized()
+	Expect(os.RemoveAll(filepath.Dir(privateKeyPath))).To(Succeed())
+
 	gexec.CleanupBuildArtifacts()
 })
 
--- scpintegration/scpintegration_test.go
+++ scpintegration/scpintegration_test.go
@@ -1,13 +1,10 @@
 package scpintegration_test
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"os/user"
 	"path/filepath"
 	"time"
 
@@ -19,15 +16,11 @@ import (
 
 var _ = Describe("SCP Backup", func() {
 	Context("When SCP server is correctly configured with flags", func() {
-		const (
-			sshKeyUsername = "service-backup-tmp-key"
-		)
-
 		var (
 			runningBin *gexec.Session
 			baseDir    string
 			destPath   string
-			unixUser   *user.User
+			flags      []string
 		)
 
 		pathWithDate := func(endParts ...string) string {
@@ -45,39 +38,23 @@ var _ = Describe("SCP Backup", func() {
 			Expect(err).NotTo(HaveOccurred())
 			dirToBackup := filepath.Join(baseDir, "source")
 			destPath = filepath.Join(baseDir, "target")
-			sshKeys := filepath.Join(baseDir, "keys")
 			Expect(os.Mkdir(dirToBackup, 0755)).To(Succeed())
-			Expect(os.Mkdir(sshKeys, 0755)).To(Succeed())
 			Expect(os.Mkdir(destPath, 0755)).To(Succeed())
-
-			unixUser, err = user.Current()
-			Expect(err).NotTo(HaveOccurred())
-
-			privateKeyPath := filepath.Join(sshKeys, "id_rsa")
-			Expect(exec.Command("ssh-keygen", "-t", "rsa", "-b", "4096", "-C", sshKeyUsername,
-				"-N", "", "-f", privateKeyPath).Run()).To(Succeed())
-
-			Expect(os.MkdirAll(filepath.Join(unixUser.HomeDir, ".ssh"), 0700)).To(Succeed())
-			authKeys, err := os.OpenFile(filepath.Join(unixUser.HomeDir, ".ssh", "authorized_keys"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-			Expect(err).ToNot(HaveOccurred())
-			pubKey, err := os.Open(filepath.Join(sshKeys, "id_rsa.pub"))
-			Expect(err).ToNot(HaveOccurred())
-			defer authKeys.Close()
-			defer pubKey.Close()
-			_, err = io.Copy(authKeys, pubKey)
-			Expect(err).ToNot(HaveOccurred())
 
 			Expect(ioutil.WriteFile(filepath.Join(dirToBackup, "1.txt"), []byte("1"), 0644)).To(Succeed())
 			Expect(os.Mkdir(filepath.Join(dirToBackup, "subdir"), 0755)).To(Succeed())
 			Expect(ioutil.WriteFile(filepath.Join(dirToBackup, "subdir", "2.txt"), []byte("2"), 0644)).To(Succeed())
 
-			cmd := exec.Command(
-				pathToServiceBackupBinary, "-source-folder", dirToBackup,
-				"-backup-creator-cmd", "ls", "-cleanup-cmd", "", "-cron-schedule", "*/5 * * * * *",
+			flags = []string{
+				"-source-folder", dirToBackup, "-backup-creator-cmd", "ls", "-cleanup-cmd", "", "-cron-schedule", "*/5 * * * * *",
 				"-ssh-host", "localhost", "-ssh-port", "22", "-ssh-user", unixUser.Username,
 				"-ssh-private-key-path", privateKeyPath, "-dest-path", destPath,
-			)
+			}
+		})
 
+		JustBeforeEach(func() {
+			var err error
+			cmd := exec.Command(pathToServiceBackupBinary, flags...)
 			runningBin, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Expect(err).NotTo(HaveOccurred())
 		})
@@ -85,20 +62,6 @@ var _ = Describe("SCP Backup", func() {
 		AfterEach(func() {
 			Expect(os.RemoveAll(baseDir)).To(Succeed())
 			Eventually(runningBin.Terminate()).Should(gexec.Exit())
-			authKeysFilePath := filepath.Join(unixUser.HomeDir, ".ssh", "authorized_keys")
-			authKeysContent, err := ioutil.ReadFile(authKeysFilePath)
-			Expect(err).NotTo(HaveOccurred())
-
-			trimmedAuthKeysLines := [][]byte{}
-			for _, line := range bytes.Split(authKeysContent, []byte("\n")) {
-				if !bytes.Contains(line, []byte(sshKeyUsername)) {
-					trimmedAuthKeysLines = append(trimmedAuthKeysLines, line)
-				}
-			}
-
-			trimmedAuthKeysContent := bytes.Join(trimmedAuthKeysLines, []byte("\n"))
-			err = ioutil.WriteFile(authKeysFilePath, trimmedAuthKeysContent, 0600)
-			Expect(err).NotTo(HaveOccurred())
 		})
 
 		It("copies files over SCP", func() {
@@ -110,6 +73,21 @@ var _ = Describe("SCP Backup", func() {
 			content2, err := ioutil.ReadFile(pathWithDate("subdir", "2.txt"))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(content2).To(Equal([]byte("2")))
+		})
+
+		Context("when not all mandatory flags are supplied for SCP to work", func() {
+			BeforeEach(func() {
+				flags = []string{
+					"-source-folder", "somedir", "-backup-creator-cmd", "ls", "-cleanup-cmd", "", "-cron-schedule", "*/5 * * * * *",
+					"-ssh-host", "localhost", "-ssh-port", "22", "-ssh-user", unixUser.Username,
+					"-dest-path", destPath,
+				}
+			})
+
+			It("exits with non-zero", func() {
+				Expect(runningBin.Wait(time.Second).ExitCode()).ToNot(Equal(0))
+				Expect(string(runningBin.Out.Contents())).To(ContainSubstring("Flag ssh-private-key-path not provided"))
+			})
 		})
 	})
 })
