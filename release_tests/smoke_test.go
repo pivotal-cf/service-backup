7f858d1088119ba13e940b96d6273735d887482f
Tom Anderson <toanderson@pivotal.io>
2016-03-14 15:51:09 +0000

    Add release test for backup storage into Azure.
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

--- release_tests/smoke_test.go
+++ release_tests/smoke_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/pivotal-cf-experimental/service-backup/s3testclient"
 
 	. "github.com/onsi/ginkgo"
@@ -24,53 +25,99 @@ var _ = Describe("smoke tests", func() {
 		boshPrivateKeyFile string
 		boshUsername       string
 		boshPassword       string
-		boshManifest       string
+		toBackup           string
 
-		client *s3testclient.S3TestClient
+		boshManifest string
 	)
 
 	BeforeEach(func() {
 		boshHost = envMustHave("BOSH_HOST")
 		boshPrivateKeyFile = envMustHave("BOSH_PRIVATE_KEY_FILE")
-		boshManifest = envMustHave("S3_BOSH_MANIFEST")
 		boshUsername = envMustHave("BOSH_USERNAME")
 		boshPassword = envMustHave("BOSH_PASSWORD")
-		awsAccessKeyID := envMustHave("AWS_ACCESS_KEY_ID")
-		awsSecretKey := envMustHave("AWS_SECRET_ACCESS_KEY")
-
-		client = s3testclient.New("https://s3-eu-west-1.amazonaws.com", awsAccessKeyID, awsSecretKey)
+		toBackup = "to_backup.txt"
 	})
 
-	AfterEach(func() {
-		Expect(client.DeleteRemotePath(bucketName, testPath)).To(Succeed())
-	})
-
-	It("Uploads files in the backup directory to S3", func() {
-		toBackup := "to_backup.txt"
-		cwd, err := os.Getwd()
-		Expect(err).NotTo(HaveOccurred())
-		pathToFile := filepath.Join(cwd, "test_assets", toBackup)
-		cmd := exec.Command(
-			"bosh",
+	boshSSH := func(command string, args ...string) {
+		commonArgs := []string{
 			"-n",
 			"-d", boshManifest,
 			"-t", fmt.Sprintf("https://%s:25555", boshHost),
 			"-u", boshUsername,
 			"-p", boshPassword,
-			"scp",
+			command,
 			"--gateway_host", boshHost,
 			"--gateway_user", "vcap",
 			"--gateway_identity_file", boshPrivateKeyFile,
-			"--upload", "service-backup/0", pathToFile, "/tmp")
+		}
+		allArgs := append(commonArgs, args...)
+		cmd := exec.Command("bosh", allArgs...)
 		cmd.Stdout = GinkgoWriter
 		cmd.Stderr = GinkgoWriter
 		Expect(cmd.Run()).To(Succeed())
+	}
 
-		// now, on the service-backups deployment, the file will be backed up to S3
+	JustBeforeEach(func() {
+		cwd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		pathToFile := filepath.Join(cwd, "test_assets", toBackup)
+		boshSSH("scp", "--upload", "service-backup/0", pathToFile, "/tmp")
+	})
 
-		Eventually(func() bool {
-			return client.RemotePathExistsInBucket(bucketName, fmt.Sprintf("%s/%s", pathWithDate(testPath), toBackup))
-		}, time.Minute).Should(BeTrue())
+	Context("backing up to S3", func() {
+		var (
+			client *s3testclient.S3TestClient
+		)
+
+		BeforeEach(func() {
+			boshManifest = envMustHave("S3_BOSH_MANIFEST")
+
+			awsAccessKeyID := envMustHave("AWS_ACCESS_KEY_ID")
+			awsSecretKey := envMustHave("AWS_SECRET_ACCESS_KEY")
+			client = s3testclient.New("https://s3-eu-west-1.amazonaws.com", awsAccessKeyID, awsSecretKey)
+		})
+
+		AfterEach(func() {
+			boshSSH("ssh", "service-backup", "0", "rm", "/tmp/"+toBackup)
+
+			Expect(client.DeleteRemotePath(bucketName, testPath)).To(Succeed())
+		})
+
+		It("Uploads files in the backup directory to S3", func() {
+			Eventually(func() bool {
+				return client.RemotePathExistsInBucket(bucketName, fmt.Sprintf("%s/%s", pathWithDate(testPath), toBackup))
+			}, time.Minute).Should(BeTrue())
+		})
+	})
+
+	Context("backing up to Azure", func() {
+		var (
+			azureBlobService storage.BlobStorageClient
+		)
+
+		BeforeEach(func() {
+			boshManifest = envMustHave("AZURE_BOSH_MANIFEST")
+			azureAccountName := envMustHave("AZURE_STORAGE_ACCOUNT")
+			azureAccountKey := envMustHave("AZURE_STORAGE_ACCESS_KEY")
+			azureClient, err := storage.NewBasicClient(azureAccountName, azureAccountKey)
+			Expect(err).ToNot(HaveOccurred())
+			azureBlobService = azureClient.GetBlobService()
+		})
+
+		AfterEach(func() {
+			boshSSH("ssh", "service-backup", "0", "rm", "/tmp/"+toBackup)
+
+			_, err := azureBlobService.DeleteBlobIfExists(bucketName, fmt.Sprintf("%s/%s", pathWithDate(testPath), toBackup))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Uploads files in the backup directory", func() {
+			Eventually(func() bool {
+				exists, err := azureBlobService.BlobExists(bucketName, fmt.Sprintf("%s/%s", pathWithDate(testPath), toBackup))
+				Expect(err).NotTo(HaveOccurred())
+				return exists
+			}, time.Minute).Should(BeTrue())
+		})
 	})
 })
 
