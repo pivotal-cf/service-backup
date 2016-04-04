a659c66aeb0227438596c876d8c60911dcdfc58a
Jonathan Qiwen Tsang <jtsang@pivotal.io>
2016-04-04 15:23:31 +0100

    Service-backup scheduler can run with no options
    This is required so that when we colocate the service backup release but
    do not enable backups for the release the service backup binary will not
    exit, causing monit to consider the deployment failed. We have
    implemented a dummy Executor to simply log "Backups Disabled" whenever
    it is triggered, we've also defaulted a cron schedule (monthly) only on
    the occasion that no destination is provided & no cron schedule is
    provided
    
    [#116695473]
    
    Signed-off-by: Simon Jones <sijones@pivotal.io>

---
+++ dummy/executor_test.go
@@ -0,0 +1,41 @@
+package dummy_test
+
+import (
+	"github.com/pivotal-cf-experimental/service-backup/backup"
+	. "github.com/pivotal-cf-experimental/service-backup/dummy"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+
+	"github.com/pivotal-golang/lager"
+)
+
+var _ = Describe("Dummy Executor", func() {
+	var dummyExecutor backup.Executor
+	var logger lager.Logger
+	var log *gbytes.Buffer
+
+	BeforeEach(func() {
+		logger = lager.NewLogger("ServiceBackup")
+		log = gbytes.NewBuffer()
+		logger.RegisterSink(lager.NewWriterSink(log, lager.INFO))
+		dummyExecutor = NewDummyExecutor(logger)
+	})
+
+	Describe("RunOnce", func() {
+		var err error
+
+		BeforeEach(func() {
+			err = dummyExecutor.RunOnce()
+		})
+
+		It("Doesn't return an error", func() {
+			Expect(err).To(BeNil())
+		})
+
+		It("Logs that backups are disabled", func() {
+			Expect(log).To(gbytes.Say("Backups Disabled"))
+		})
+	})
+})
