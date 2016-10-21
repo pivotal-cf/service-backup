83c48763d992ad3f6dce37dcc1777a62867fbdb4
Josh Hill <jhill@pivotal.io>
2016-10-21 14:32:28 +0100

    Remove dot imports in config parse test
    [#121862563]
    
    Signed-off-by: Sam Gunaratne <rgunaratne@pivotal.io>

--- config/parse_test.go
+++ config/parse_test.go
@@ -3,20 +3,20 @@ package config_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/pivotal-cf-experimental/service-backup/backup"
-	. "github.com/pivotal-cf-experimental/service-backup/config"
-	. "github.com/pivotal-cf/service-alerts-client/client"
+	"github.com/pivotal-cf-experimental/service-backup/backup"
+	"github.com/pivotal-cf-experimental/service-backup/config"
+	alerts "github.com/pivotal-cf/service-alerts-client/client"
 )
 
 var _ = Describe("Parsing properties", func() {
 	Context("Valid backup config with alerts configured", func() {
 		var (
 			cron     string
-			executor Executor
-			alerts   *ServiceAlertsClient
+			executor backup.Executor
+			alerts   *alerts.ServiceAlertsClient
 		)
 		BeforeEach(func() {
-			executor, cron, alerts, _ = Parse([]string{"cmd", "fixtures/valid_backup_with_alerts.yml"})
+			executor, cron, alerts, _ = config.Parse([]string{"cmd", "fixtures/valid_backup_with_alerts.yml"})
 		})
 
 		It("has the correct cron", func() {
@@ -33,15 +33,14 @@ var _ = Describe("Parsing properties", func() {
 	})
 
 	Context("Valid backup config without alerts configured", func() {
-		var alerts *ServiceAlertsClient
+		var alerts *alerts.ServiceAlertsClient
 
 		BeforeEach(func() {
-			_, _, alerts, _ = Parse([]string{"cmd", "fixtures/valid_backup_without_alerts.yml"})
+			_, _, alerts, _ = config.Parse([]string{"cmd", "fixtures/valid_backup_without_alerts.yml"})
 		})
 
 		It("returns no alerts client", func() {
 			Expect(alerts).To(BeNil())
 		})
 	})
-
 })
