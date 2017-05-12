a7fd9731b0cef82f35470d8d0bc4eab0c8ff25dc
Jake Klein <jklein@pivotal.io>
2017-05-12 10:11:22 +0100

    Test gcp, scp and azure clients for intalize
    [#144382085]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- upload/initialize_test.go
+++ upload/initialize_test.go
@@ -6,8 +6,11 @@ import (
 	"code.cloudfoundry.org/lager"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/pivotal-cf/service-backup/azure"
 	"github.com/pivotal-cf/service-backup/config"
+	"github.com/pivotal-cf/service-backup/gcp"
 	"github.com/pivotal-cf/service-backup/s3"
+	"github.com/pivotal-cf/service-backup/scp"
 	"github.com/pivotal-cf/service-backup/upload"
 	"github.com/pivotal-cf/service-backup/upload/fakes"
 )
@@ -20,75 +23,112 @@ var _ = Describe("Initialize", func() {
 
 	BeforeEach(func() {
 		factory = new(fakes.FakeUploaderFactory)
-		factory.S3Returns(s3.New("fake", "", "", "", "", "", "", nil))
 		logger = lager.NewLogger("parser")
 		logger.RegisterSink(lager.NewWriterSink(GinkgoWriter, lager.DEBUG))
 	})
 
-	Context("when S3 is configured", func() {
+	Context("when generating an S3 uploader", func() {
 		It("returns a list of 1 backuper", func() {
 			expectedCACert := "/path/to/my/cert"
-			expectedDestination := config.Destination{
-				Type: "s3",
-				Config: map[string]interface{}{
-					"bucket_name":       "some-bucket",
-					"bucket_path":       "some-bucket-path",
-					"endpoint_url":      "some-endpoint-url",
-					"region":            "a-region",
-					"access_key_id":     "some-access-key-id",
-					"secret_access_key": "some-secret-access-key",
-				},
-			}
-
-			backupConfig := &config.BackupConfig{
-				Destinations: []config.Destination{expectedDestination},
-			}
-
-			uploader, err := upload.Initialize(backupConfig, logger, upload.WithUploaderFactory(factory), upload.WithCACertLocator(func() (string, error) {
+			fakeCACertLocator := func() (string, error) {
 				return expectedCACert, nil
-			}))
+			}
+			backupConfig := backupConfig("s3")
+			factory.S3Returns(s3.New("s3", "", "", "", "", "", "", nil))
 
-			Expect(uploader).NotTo(BeNil())
+			uploader, err := upload.Initialize(backupConfig, logger, upload.WithUploaderFactory(factory), upload.WithCACertLocator(fakeCACertLocator))
+
 			Expect(err).NotTo(HaveOccurred())
-
-			Expect(factory.AzureCallCount()).To(Equal(0))
-			Expect(factory.SCPCallCount()).To(Equal(0))
-			Expect(factory.GCPCallCount()).To(Equal(0))
+			Expect(uploader).NotTo(BeNil())
+			Expect(uploader.Name()).To(Equal("multi-uploader: s3"))
 			Expect(factory.S3CallCount()).To(Equal(1))
-
 			dest, caCert := factory.S3ArgsForCall(0)
-			Expect(dest).To(Equal(expectedDestination))
+			Expect(dest).To(Equal(backupConfig.Destinations[0]))
 			Expect(caCert).To(Equal(expectedCACert))
+		})
 
-			Expect(uploader.Name()).To(Equal("multi-uploader: fake"))
+		Context("when the ca cert lookup fails", func() {
+			It("returns an error", func() {
+				backupConfig := backupConfig("s3")
+				expectedErr := errors.New("failed")
+				fakeCACertLocator := func() (string, error) {
+					return "", expectedErr
+				}
+
+				_, err := upload.Initialize(backupConfig, logger, upload.WithCACertLocator(fakeCACertLocator))
+
+				Expect(err).To(Equal(expectedErr))
+			})
 		})
 	})
 
-	Context("when the ca cert lookup fails", func() {
-		It("returns an error", func() {
-			expectedErr := errors.New("failed")
+	Context("when generating an SCP uploader", func() {
+		It("returns a list of 1 backuper", func() {
+			backupConfig := backupConfig("scp")
+			factory.SCPReturns(scp.New("scp", "", 0, "", "", "", nil))
 
-			conf := &config.BackupConfig{
-				Destinations: []config.Destination{{Type: "s3"}},
-			}
+			uploader, err := upload.Initialize(backupConfig, logger, upload.WithUploaderFactory(factory), upload.WithCACertLocator(noopCACertLocator))
 
-			_, err := upload.Initialize(conf, logger, upload.WithCACertLocator(func() (string, error) {
-				return "", expectedErr
-			}))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(uploader).NotTo(BeNil())
+			Expect(uploader.Name()).To(Equal("multi-uploader: scp"))
+			Expect(factory.SCPCallCount()).To(Equal(1))
+			Expect(factory.SCPArgsForCall(0)).To(Equal(backupConfig.Destinations[0]))
+		})
+	})
 
-			Expect(err).To(Equal(expectedErr))
+	Context("when generating an Azure uploader", func() {
+		It("returns a list of 1 backuper", func() {
+			backupConfig := backupConfig("azure")
+			factory.AzureReturns(azure.New("azure", "", "", "", "", "", nil))
+
+			uploader, err := upload.Initialize(backupConfig, logger, upload.WithUploaderFactory(factory), upload.WithCACertLocator(noopCACertLocator))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(uploader).NotTo(BeNil())
+			Expect(uploader.Name()).To(Equal("multi-uploader: azure"))
+			Expect(factory.AzureCallCount()).To(Equal(1))
+			Expect(factory.AzureArgsForCall(0)).To(Equal(backupConfig.Destinations[0]))
+		})
+	})
+
+	Context("when generating an GCP uploader", func() {
+		It("returns a list of 1 backuper", func() {
+			backupConfig := backupConfig("gcs")
+			factory.GCPReturns(gcp.New("gcs", "", "", "", nil))
+
+			uploader, err := upload.Initialize(backupConfig, logger, upload.WithUploaderFactory(factory), upload.WithCACertLocator(noopCACertLocator))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(uploader).NotTo(BeNil())
+			Expect(uploader.Name()).To(Equal("multi-uploader: gcs"))
+			Expect(factory.GCPCallCount()).To(Equal(1))
+			Expect(factory.GCPArgsForCall(0)).To(Equal(backupConfig.Destinations[0]))
 		})
 	})
 
 	Context("when an unknown destination type is configured", func() {
 		It("returns an error", func() {
-			backupConfig := &config.BackupConfig{
-				Destinations: []config.Destination{
-					{Type: "unknown-type"},
-				},
-			}
+			backupConfig := backupConfig("unknown-type")
+
 			_, err := upload.Initialize(backupConfig, logger)
+
 			Expect(err).To(MatchError("unknown destination type: unknown-type"))
 		})
 	})
 })
+
+func backupConfig(destType string) *config.BackupConfig {
+	dest := config.Destination{
+		Type:   destType,
+		Config: map[string]interface{}{},
+	}
+	backupConfig := &config.BackupConfig{
+		Destinations: []config.Destination{dest},
+	}
+	return backupConfig
+}
+
+func noopCACertLocator() (string, error) {
+	return "", nil
+}
