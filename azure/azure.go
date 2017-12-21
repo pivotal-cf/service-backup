e0c7e9d1ca71a21e27758fbe5829042b94137aae
Andrea Nodari <anodari@pivotal.io>
2017-12-21 12:25:27 +0000

    Blobxfer 1.0 command has changed
    [#153317656]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>

--- azure/azure.go
+++ azure/azure.go
@@ -8,9 +8,9 @@ package azure
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 
 	"code.cloudfoundry.org/lager"
 )
@@ -43,34 +43,10 @@ func (a *AzureClient) Upload(localPath string, sessionLogger lager.Logger) error
 	sessionLogger.Info("Uploading azure blobs", lager.Data{"container": a.container, "localPath": localPath, "remotePath": remotePath})
 	sessionLogger.Info("The container and remote path will be created if they don't already exist", lager.Data{"container": a.container, "remotePath": remotePath})
 	sessionLogger.Info(fmt.Sprintf("about to upload %s to Azure remote path %s", localPath, remotePath))
-	return a.uploadDirectory(localPath, remotePath)
+	return a.uploadDir(localPath, remotePath)
 }
 
-func (a *AzureClient) uploadDirectory(localDirPath, remoteDirPath string) error {
-	localFiles, err := ioutil.ReadDir(localDirPath)
-	if err != nil {
-		return err
-	}
-
-	for _, localFile := range localFiles {
-		fileName := localFile.Name()
-		localFilePath := localDirPath + "/" + fileName
-		remoteFilePath := remoteDirPath + "/" + fileName
-
-		if localFile.IsDir() {
-			err = a.uploadDirectory(localFilePath, remoteFilePath)
-		} else {
-			length := uint64(localFile.Size())
-			err = a.uploadFile(localFilePath, remoteFilePath, length)
-		}
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (a *AzureClient) uploadFile(localFilePath, remoteFilePath string, length uint64) error {
+func (a *AzureClient) uploadDir(localFilePath, remoteFilePath string) error {
 	file, err := os.Open(localFilePath)
 	if err != nil {
 		return err
@@ -78,8 +54,17 @@ func (a *AzureClient) uploadFile(localFilePath, remoteFilePath string, length ui
 
 	defer file.Close()
 
-	cmd := exec.Command(a.azureCmd, fmt.Sprintf("--remoteresource=%s", remoteFilePath), a.accountName, a.container, localFilePath)
-	cmd.Env = append(cmd.Env, fmt.Sprintf("BLOBXFER_STORAGEACCOUNTKEY=%s", a.accountKey))
+	cmd := exec.Command(
+		a.azureCmd, "upload",
+		"--local-path", localFilePath,
+		"--remote-path", path.Join(a.container, remoteFilePath),
+		"--storage-account", a.accountName)
+
+	cmd.Env = append(cmd.Env,
+		"LANG="+os.Getenv("LANG"),
+		"LC_ALL="+os.Getenv("LC_ALL"),
+		"BLOBXFER_STORAGE_ACCOUNT_KEY="+a.accountKey,
+	)
 	return cmd.Run()
 }
 
